d9dc7a3b6eab86b93cc709aea0d319cfb68fd5c5
xiao <cangzhishang@gmail.com>
2024-08-06 22:40:29 -0500

    djangoBlog using bootstrap for the style of detailed page initial

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+
 from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.index,name='index'),
+    path('', views.index, name='index'),
+    path('blog/<blog_id>', views.blog_detail, name='blog_detail')
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
+
+def blog_detail(request, blog_id):
+    return render(request, 'blog_detail.html')
