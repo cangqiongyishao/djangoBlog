056a55d246eae6f306f186bdd958522685f3d358
xiao <cangzhishang@gmail.com>
2024-08-11 19:28:08 -0500

    post blog backend part done

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
 from django.urls.base import reverse_lazy
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
+from .models import BlogCategory, BlogComment, Blog
+from .forms import PubBlogForm
 
 
 # Create your views here.
@@ -15,6 +19,21 @@ def blog_detail(request, blog_id):
 
 # @login_required(login_url=reverse_lazy('xiaoauth:login'))
 # @login_required(login_url=reverse_lazy('/auth/login'))
+
+@require_http_methods(['GET', 'POST'])
 @login_required()
 def pub_blog(request):
-    return render(request, 'pub_blog.html')
+    if request.method == 'GET':
+        categories = BlogCategory.objects.all()
+        return render(request, 'pub_blog.html', context={'categories': categories})
+    else:
+        form = PubBlogForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data.get('title')
+            content = form.cleaned_data.get('content')
+            category_id = form.cleaned_data.get('category')
+            Blog.objects.create(title=title, content=content, category_id=category_id, author=request.user)
+            return JsonResponse({'Code': 200, 'message': 'Blog post successfully!'})
+        else:
+            print(form.errors)
+            return JsonResponse({'code': 400, 'message': 'Invalid parameter'})
